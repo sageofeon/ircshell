3e556b3f364f6e78fa21f80427ca1bf62b660fd8
sergebash <sergebash@gmail.com>
2021-09-04 00:00:31 +0300

    Update mainc

--- main.c
+++ main.c
@@ -1,15 +1,3 @@
-/*[ hexbot IRC bot ][ 2013 ]
-*
-*	Written by Bashlykov Sergey aka hexfox
-*
-*   hexbot - Remote control of a Windows computer via IRC.
-*   The IRC bot connects to the IRC server and enters
-*   the channel. When another user sends a private message
-*   to the bot, this message as a command is executed
-*   on the remote computer where the bot started.
-*
-*/
-
 #include "functions.h"
 
  /*
